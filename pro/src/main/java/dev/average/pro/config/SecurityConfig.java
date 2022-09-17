7d842a346083a7eb43a30c228f24c16c78204b11
Ömer Aran <67416117+OmerAran@users.noreply.github.com>
2022-09-17 15:06:46 +0300

    securtiyconfig added

--- pro/src/main/java/dev/average/pro/config/SecurityConfig.java
+++ pro/src/main/java/dev/average/pro/config/SecurityConfig.java
@@ -14,16 +14,13 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/", "/forms");
-    }
+    
 
 }
 
