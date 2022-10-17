89325ef0ba9add3823b502b9004c94e4f5698ad3
Ömer Aran <67416117+OmerAran@users.noreply.github.com>
2022-10-17 16:24:55 +0300

    routing handled

--- pro-angular/src/app/app-routing.module.ts
+++ pro-angular/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProComponent } from './components/pro/pro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'form', component: FormComponent},
+  {path:'pro/:id',component:ProComponent},
+  {path:'**',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
