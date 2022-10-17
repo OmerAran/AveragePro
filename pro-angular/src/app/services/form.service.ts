3a8d400f3ff7d3fbfe2c770a35f8d17a6113828c
Ömer Aran <67416117+OmerAran@users.noreply.github.com>
2022-10-17 16:23:23 +0300

    form service added

--- pro-angular/src/app/services/form.service.ts
+++ pro-angular/src/app/services/form.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+const baseUrl  = 'http://localhost:8080';
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getAvgSalaryById(id : any){
+   return  this.http.get(`${baseUrl}/pro/avgsalary/${id}`);
+  }
+  getAvgWorkEnvById(id : any){
+    return  this.http.get(`${baseUrl}/pro/avgenv/${id}`);
+   }
+   getAvgWorkWeekById(id : any){
+    return  this.http.get(`${baseUrl}/pro/avgweek/${id}`);
+   }
 }
